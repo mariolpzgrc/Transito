9575c35d81ca265af4d876c3db40b695e43e4106
ferguzaja <ferguzaja@gmail.com>
2019-06-11 18:22:33 -0500

    solucion login

--- TransitoAplicacionWeb/Transito/Controllers/Usuario.cs
+++ TransitoAplicacionWeb/Transito/Controllers/Usuario.cs
@@ -16,48 +16,5 @@ namespace Transito.Controllers
             return View();
         }
 
-        [HttpPost]
-        public IActionResult Validar(String login, String password)
-        {
-            Usuario usuario = ValidarUsuario(login, password);
-            bool valido = usuario != null;
-            if (valido)
-            {
-                using (TransitoContext dbSS = new TransitoContext())
-                {
-                    String token = "";
-                    do
-                    {
-                        token = GenerarToken();
-                    } while (dbSS.UsuarioBitacoraAcceso.Any(a => a.TokenAcceso.Equals(token)));
-
-                    UsuarioBitacoraAcceso registroAcceso = new UsuarioBitacoraAcceso();
-                    registroAcceso.Fecha = DateTime.Now;
-                    registroAcceso.Idusuario = usuario.Id;
-                    registroAcceso.TokenAcceso = token;
-                    registroAcceso.Activa = true;
-                    dbSS.UsuarioBitacoraAcceso.Add(registroAcceso);
-                    dbSS.SaveChanges();
-
-                    byte[] arr = BitConverter.GetBytes(registroAcceso.Id);
-                    HttpContext.Session.Set("SesionUsuario", arr);
-                    HttpContext.Session.Set("Usuario",
-                     Encoding.ASCII
-                     .GetBytes($"{usuario.Nombre}"));
-                    return new RedirectResult("/Administrador/Seguimiento");
-                }
-            }
-            else
-                return new RedirectResult("/");
-        }
-
-        private String GenerarToken()
-        {
-            Random random = new Random();
-            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz";
-            return new string(Enumerable.Repeat(chars, 30)
-                    .Select(s => s[random.Next(s.Length)]).ToArray());
-
-        }
-    }
+    }   
 }
