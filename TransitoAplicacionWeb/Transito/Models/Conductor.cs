3ffc9410360b8b24e30bd044e06c89192c6d1ff7
LUNIXINC\lunix <lunix_king@hotmail.com>
2019-06-11 19:06:20 -0500

    Se agregaron los pojos

---
+++ TransitoAplicacionWeb/Transito/Models/Conductor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Transito.Models
+{
+    public partial class Conductor
+    {
+        public Conductor()
+        {
+            Vehiculo = new HashSet<Vehiculo>();
+        }
+
+        public int Idconductor { get; set; }
+        public string Nombre { get; set; }
+        public string ApellidoPaterno { get; set; }
+        public string ApellidoMaterno { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string NumeroLicencia { get; set; }
+        public string Telefono { get; set; }
+        public string Password { get; set; }
+
+        public ICollection<Vehiculo> Vehiculo { get; set; }
+    }
+}
