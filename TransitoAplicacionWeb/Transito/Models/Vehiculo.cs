b6681eabfe5d1078fe868c18fe82789fca6a3a57
LUNIXINC\lunix <lunix_king@hotmail.com>
2019-06-11 19:08:57 -0500

    Otra parte de as entedidades

---
+++ TransitoAplicacionWeb/Transito/Models/Vehiculo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Transito.Models
+{
+    public partial class Vehiculo
+    {
+        public string Placas { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public int Anio { get; set; }
+        public string Color { get; set; }
+        public int Idaseguradora { get; set; }
+        public string PolizaSeguro { get; set; }
+        public int Idconductor { get; set; }
+
+        public Aseguradora IdaseguradoraNavigation { get; set; }
+        public Conductor IdconductorNavigation { get; set; }
+    }
+}
