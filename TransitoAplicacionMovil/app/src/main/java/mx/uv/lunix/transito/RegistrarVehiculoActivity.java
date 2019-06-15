90141a2946581b8f3695f7247b9ba2730d0fe804
LUNIXINC\lunix <lunix_king@hotmail.com>
2019-06-14 23:29:04 -0500

    Se agregaron mas funcionalidades

--- TransitoAplicacionMovil/app/src/main/java/mx/uv/lunix/transito/RegistrarVehiculoActivity.java
+++ TransitoAplicacionMovil/app/src/main/java/mx/uv/lunix/transito/RegistrarVehiculoActivity.java
@@ -2,12 +2,57 @@ package mx.uv.lunix.transito;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import mx.uv.lunix.transito.ws.Response;
+import mx.uv.lunix.transito.ws.pojos.Aseguradora;
+import mx.uv.lunix.transito.ws.pojos.Vehiculo;
 
 public class RegistrarVehiculoActivity extends AppCompatActivity {
+
+    private Vehiculo nuevo;
+    private Aseguradora nueva;
+    private EditText placa;
+    private EditText marca;
+    private EditText modelo;
+    private EditText color;
+    private EditText anio;
+    private EditText numeropoliza;
+    private EditText aseguradora;
+    private Response resws;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrar_vehiculo);
+        placa = (EditText) findViewById(R.id.numerodePlacas);
+        marca = (EditText) findViewById(R.id.marca);
+        modelo = (EditText) findViewById(R.id.modelos);
+        color = (EditText) findViewById(R.id.color);
+        anio = (EditText) findViewById(R.id.año);
+        numeropoliza = (EditText) findViewById(R.id.poliza);
+        aseguradora = (EditText) findViewById(R.id.aseguradora);
+        WSPOSTRegistroVehiculoTask task = new WSPOSTRegistroVehiculoTask();
+        task.execute(nuevo);
+        WSPOSTRegitroAseguradoraTask task = new WSPOSTRegistroAseguradoraTask();
+        task.execute(nueva);
+    }
+
+    public void cancelar(View v){finish();}
+
+    private void registrarVehiculo(View v){
+        if(validar() && isOnline){
+            nuevo = new Vehiculo();
+            nueva = new Aseguradora();
+            nuevo.setMarca(marca.getText().toString());
+            nuevo.setModelo(modelo.getText().toString());
+            nuevo.setAnio(Integer.parseInt(anio.getText().toString()));
+            nuevo.setColor(color.getText().toString());
+            nuevo.setPlacas(placa.getText().toString());
+
+
+        }
     }
 }
--- TransitoAplicacionMovil/app/src/main/java/mx/uv/lunix/transito/RegistroActivity.java
+++ TransitoAplicacionMovil/app/src/main/java/mx/uv/lunix/transito/RegistroActivity.java
@@ -1,10 +1,19 @@
 package mx.uv.lunix.transito;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.os.AsyncTask;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
+import com.google.gson.Gson;
+
+import mx.uv.lunix.transito.ws.HttpUtils;
 import mx.uv.lunix.transito.ws.Response;
 import mx.uv.lunix.transito.ws.pojos.Conductor;
 
@@ -18,7 +27,7 @@ public class RegistroActivity extends AppCompatActivity {
     private EditText numerolicencia;
     private EditText numerocelular;
     private EditText contrasena;
-    private Response rews;
+    private Response resws;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,15 +45,117 @@ public class RegistroActivity extends AppCompatActivity {
     public void cancelar(View v){finish();}
 
     public void registrarme(View view){
-        if(validar() && isOnline){
+        if(validar() && isOnline()){
             nuevo = new Conductor();
             nuevo.setNombre(nombre.getText().toString());
             nuevo.setApellidoMaterno(apellidopaterno.getText().toString());
             nuevo.setApellidoMaterno(apellidoMaterno.getText().toString());
             nuevo.setFechaNacimiento(fechanacimiento.getText().toString());
-            
+            nuevo.setNumeroLicencia(numerolicencia.getText().toString());
+            nuevo.setTelefono(numerocelular.getText().toString());
+            nuevo.setPassword(contrasena.getText().toString());
+            WSPOSTRegistroConductorTask task = new WSPOSTRegistroConductorTask();
+            task.execute(nuevo);
 
         }
+    }
+
+    private boolean validar(){
+        boolean ok = true;
+        if(nombre.getText()== null || nombre.getText().toString().isEmpty()){
+            nombre.setError("Debes introducir tu(s) nombre(s)");
+            ok = false;
+        }
+
+        if(apellidopaterno.getText() == null || apellidopaterno.getText().toString().isEmpty()){
+            apellidopaterno.setError("Debes de introducir tu apellido");
+            ok = false;
+        }
+        if(apellidoMaterno.getText() == null || apellidoMaterno.getText().toString().isEmpty()){
+            apellidoMaterno.setError("Debes de introducir tu apellido");
+            ok = false;
+        }
+        if(fechanacimiento.getText() == null || fechanacimiento.getText().toString().isEmpty()){
+            fechanacimiento.setError("Debes de incluir tu fecha de nacimiento");
+            ok = false;
+        }
+        if(numerolicencia.getText() == null || numerolicencia.getText().toString().isEmpty()){
+            numerolicencia.setError("Debes de introducir tu número de licencia.");
+            ok = false;
+        }
+        if(numerocelular.getText() == null || numerocelular.getText().toString().isEmpty()){
+            numerocelular.setError("Debes de introducir tu número de celular");
+            ok = false;
+        }
+        if(contrasena.getText() == null || contrasena.getText().toString().isEmpty()){
+            contrasena.setError("Debes de introducir tu número de celular");
+            ok = false;
+        }
+        return ok;
+    }
+
+    private boolean isOnline() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        boolean online = (cm.getActiveNetworkInfo() != null && cm.getActiveNetworkInfo().isConnected());
+        if(!online) {
+            mostrarAlertDialog("Sin conexión", "No se encontró ninguna conexión a Internet, conectase a alguna red para continuar utilizando la aplicación");
+        }
+        return online;
+    }
+
+    private void mostrarAlertDialog(String titulo, String mensaje) {
+        AlertDialog dialog = new AlertDialog.Builder(RegistroActivity.this).create();
+        dialog.setTitle(titulo);
+        dialog.setMessage(mensaje);
+        dialog.setButton(AlertDialog.BUTTON_POSITIVE, "Aceptar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
+    }
+    private class WSPOSTLoginTask extends AsyncTask<String, String, String> {
+        @Override
+        protected String doInBackground(String... strings) {
+            resws = HttpUtils.login(strings[0], strings[1]);
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            resultadoEntrar();
+        }
+    }
+    private void resultadoEntrar() {
+        if (resws != null && !resws.isError() && resws.getResult() != null) {
+            if (resws.getResult().contains("idconductor")) {
+                Intent intent = new Intent(RegistroActivity.this, PrincipalPaginaActivity.class);
+                intent.putExtra("json_conductor", resws.getResult());
+                startActivity(intent);
+            }
+        } else {
+            mostrarAlertDialog("Error", "Número celular y/o contraseña incorrectas.");
+        }
+    }
+
+    private class WSPOSTRegistroConductorTask extends AsyncTask<Object, String, String> {
+        @Override
+        protected String doInBackground(Object... objects) {
+            resws = HttpUtils.registroConductor((Conductor)objects[0]);
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            resultadoRegistro();
+        }
+    }
+
+    private void resultadoRegistro(){
+
     }
 
 }
