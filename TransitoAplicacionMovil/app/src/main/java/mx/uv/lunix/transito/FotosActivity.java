ef021a00a02d5fecfde31988fed7e881f021be81
LUNIXINC\lunix <lunix_king@hotmail.com>
2019-06-17 18:59:07 -0500

    Se corriegieron partes

--- TransitoAplicacionMovil/app/src/main/java/mx/uv/lunix/transito/FotosActivity.java
+++ TransitoAplicacionMovil/app/src/main/java/mx/uv/lunix/transito/FotosActivity.java
@@ -1,7 +1,14 @@
 package mx.uv.lunix.transito;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import mx.uv.lunix.transito.ws.Responses;
 
 public class FotosActivity extends AppCompatActivity {
 
@@ -9,5 +16,15 @@ public class FotosActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fotos);
+        foto1 = (ImageView) findViewById(R.id.imageView1);
+        foto2 = (ImageView) findViewById(R.id.imageView2);
+        foto3 = (ImageView) findViewById(R.id.imageView3);
+        foto4 = (ImageView) findViewById(R.id.imageView4);
+        foto5 = (ImageView) findViewById(R.id.imageView5);
+        foto6 = (ImageView) findViewById(R.id.imageView6);
+        foto7 = (ImageView) findViewById(R.id.imageView7);
+        foto8 = (ImageView) findViewById(R.id.imageView8);
+
+
     }
 }
